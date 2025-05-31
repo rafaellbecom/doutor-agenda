e6c1589c2a7144662e66ea6f859f4df46c41fc0f
Rafael <rafaellbecom@gmail.com>
2025-05-31 02:45:45 -0300

    feat: add login form

--- src/app/authentication/components/login-form.tsx
+++ src/app/authentication/components/login-form.tsx
@@ -24,25 +24,23 @@ import {
   import { Input } from "@/components/ui/input"
   import { Label } from "@/components/ui/label"
 
-const registerSchema = z.object({
-    name: z.string().trim().min(1, { message: "Nome é obrigatório."}),
+const loginSchema = z.object({
     email: z.string().trim().min(1, {message: "Email é obrigatório."}).email({ message: "Email inválido"}),
     password: z.string().trim().min(8, {message: "Senha deve conter ao menos 8 caracteres."}),
 })
 
-const SignUpForm = () => {
+const LoginForm = () => {
     // 1. Define your form.
-    const form = useForm<z.infer<typeof registerSchema>>({
-        resolver: zodResolver(registerSchema),
+    const form = useForm<z.infer<typeof loginSchema>>({
+        resolver: zodResolver(loginSchema),
         defaultValues: {
-          name: "",
           email: "",
           password: "",
         },
       })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof registerSchema>) {
+    function onSubmit(values: z.infer<typeof loginSchema>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         console.log(values)
@@ -52,26 +50,12 @@ const SignUpForm = () => {
         <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
           <CardHeader>
-            <CardTitle>Criar Conta</CardTitle>
+            <CardTitle>Login</CardTitle>
             <CardDescription>
-              Crie uma conta para continuar.
+              Faça login para continuar.
             </CardDescription>
           </CardHeader>
           <CardContent className="space-y-4">
-                      <FormField
-                      control={form.control}
-                      name="name"
-                      render={({ field }) => (
-                          <FormItem>
-                          <FormLabel>Nome</FormLabel>
-                          <FormControl>
-                              <Input placeholder="Digite seu nome" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                          </FormItem>
-                      )}
-                      />
-
                       <FormField
                       control={form.control}
                       name="email"
@@ -101,11 +85,11 @@ const SignUpForm = () => {
                       />
           </CardContent>
           <CardFooter>
-              <Button type="submit" className="w-full">Criar Conta</Button>
+              <Button type="submit" className="w-full">Entrar</Button>
           </CardFooter>
           </form>
       </Form>
         </Card> );
 }
  
-export default SignUpForm;
+export default LoginForm;
--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -17,31 +17,19 @@ import {
 } from "@/components/ui/tabs"
 import { Button } from "@/components/ui/button"
 import SignUpForm from "./components/sign-up-form"
+import LoginForm from "./components/login-form"
 
 const AuthenticationPage = () => {
     return <div className="h-screen w-screen flex items-center justify-center">
 
     <div className="flex w-full max-w-sm flex-col gap-6">
-      <Tabs defaultValue="register">
+      <Tabs defaultValue="login">
         <TabsList>
           <TabsTrigger value="account">Login</TabsTrigger>
           <TabsTrigger value="password">Criar Conta</TabsTrigger>
         </TabsList>
         <TabsContent value="login">
-          <Card>
-            <CardHeader>
-              <CardTitle>Login</CardTitle>
-              <CardDescription>
-                Faça login para continuar.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-6">
-            
-            </CardContent>
-            <CardFooter>
-              <Button>Save changes</Button>
-            </CardFooter>
-          </Card>
+          <LoginForm/>
         </TabsContent>
         <TabsContent value="register">
           <SignUpForm/>
