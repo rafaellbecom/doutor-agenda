a1281e3640c7589a1253490cfe519d46c25b3aa0
Rafael <rafaellbecom@gmail.com>
2025-06-04 00:07:42 -0300

    fix: add optional chaining for user and clinic properties in sidebar and doctors page

--- src/app/(protected)/components/app-sidebar.tsx
+++ src/app/(protected)/components/app-sidebar.tsx
@@ -104,9 +104,11 @@ export function AppSidebar() {
                     <AvatarFallback>R</AvatarFallback>
                   </Avatar>
                   <div>
-                    <p className="text-sm">{session.data?.user.clinic.name}</p>
+                    <p className="text-sm">
+                      {session.data?.user?.clinic?.name}
+                    </p>
                     <p className="text-muted-foreground text-sm">
-                      {session.data?.user.email}
+                      {session.data?.user?.email}
                     </p>
                   </div>
                 </SidebarMenuButton>
